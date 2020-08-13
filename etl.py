a955d6cbc70c438919dc9341affca7b4fd3c83de
Clement <dapsen59@gmail.com>
2020-08-12 23:59:35 -0500

    Add files via upload

--- etl.py
+++ etl.py
@@ -1,94 +1,97 @@
+import configparser
+from datetime import datetime
 import os
-import glob
-import psycopg2
-import pandas as pd
-from sql_queries import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
 
-def process_song_file(cur, filepath):
-    # open song file
-    df =  pd.read_json(filepath,lines=True)
+config = configparser.ConfigParser()
+config.read('dl.cfg')
 
-    # insert song record
-    song_data = list(df[['song_id','title','artist_id','year','duration']].values[0])
-    cur.execute(song_table_insert, song_data)
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    song_data = 'data'
     
-    # insert artist record
-    artist_data = list(df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0])
-    cur.execute(artist_table_insert, artist_data)
+    # read song data file
+    df = spark.read.csv("s3://udacity-dend/song_data")
+    df.printSchema()
 
-
-def process_log_file(cur, filepath):
-    # open log file
-    df = pd.read_json(filepath,lines=True)
-
-    # filter by NextSong action
-    df = df[df.page.eq('NextSong')]
-
-    # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'], unit ='ms')
+    # extract columns to create songs table
+  #  songs_table = 
     
-    # insert time data records
-    time_data = (t.dt.time,t.dt.hour,t.dt.day,t.dt.week,t.dt.month,t.dt.year,t.dt.weekday_name)
-    column_labels =  ('Start_time','hour','day','weekofyear','month','year','weekday')
-    time_df = pd.DataFrame(dict(zip(column_labels,time_data)))
+    
+    # write songs table to parquet files partitioned by year and artist
+   # songs_table
 
-    for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+    # extract columns to create artists table
+  #  artists_table = 
+    
+    # write artists table to parquet files
+    artists_table
 
-    # load user table
-    user_df = pd.DataFrame({'userid':df.userId,'firstName':
-    df.firstName,'lastName':df.lastName,'gender':df.gender,'level':df.level})
+'''
+def process_log_data(spark, input_data, output_data):
+    # get filepath to log data file
+    log_data =
 
-    # insert user records
-    for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+    # read log data file
+    df = 
+    
+    # filter by actions for song plays
+    df = 
 
-    # insert songplay records
-    for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fetchone()
-        
-        if results:
-            songid, artistid = results
-        else:
-            songid, artistid = None, None
+    # extract columns for users table    
+    artists_table = 
+    
+    # write users table to parquet files
+    artists_table
 
-        # insert songplay record
-        songplay_data = for index, row in df.iterrows():
-        cur.execute(songplay_table_insert, songplay_data)
+    # create timestamp column from original timestamp column
+    get_timestamp = udf()
+    df = 
+    
+    # create datetime column from original timestamp column
+    get_datetime = udf()
+    df = 
+    
+    # extract columns to create time table
+    time_table = 
+    
+    # write time table to parquet files partitioned by year and month
+    time_table
 
+    # read in song data to use for songplays table
+    song_df = 
 
-def process_data(cur, conn, filepath, func):
-    # get all files matching extension from directory
-    all_files = []
-    for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
-            all_files.append(os.path.abspath(f))
+    # extract columns from joined song and log datasets to create songplays table 
+    songplays_table = 
 
-    # get total number of files found
-    num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
-
-    # iterate over files and process
-    for i, datafile in enumerate(all_files, 1):
-        func(cur, datafile)
-        conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
-    conn.close()
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = ""
+    
+    process_song_data(spark, input_data, output_data)    
+    process_log_data(spark, input_data, output_data)
 
 
 if __name__ == "__main__":
     main()
+'''
